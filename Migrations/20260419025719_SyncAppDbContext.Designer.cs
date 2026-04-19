aa42fa9fa1f3f34b19c42857491dcff1953876eb
JimmyNguyen09-AI <ng.trungthanh04@gmail.com>
2026-04-19 13:07:01 +1000

    fix deploy bus

--- Migrations/20260419025719_SyncAppDbContext.Designer.cs
+++ Migrations/20260419025719_SyncAppDbContext.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using skillexa_backend.Infrastructure.Data;
@@ -12,9 +13,11 @@ using skillexa_backend.Infrastructure.Data;
 namespace skillexa_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260419025719_SyncAppDbContext")]
+    partial class SyncAppDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260419025719_SyncAppDbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace skillexa_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncAppDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "badges",
+                table: "users",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new List<string>());
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "last_streak_date_utc",
+                table: "users",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "streak_days",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "xp",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "badges",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_streak_date_utc",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "streak_days",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "xp",
+                table: "users");
+        }
+    }
+}
